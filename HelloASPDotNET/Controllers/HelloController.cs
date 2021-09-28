4f4ef1d14c7296fa69ce7caf85de6c7f8574bf47
andrew <andrewjharris96@gmail.com>
2021-09-28 15:30:35 -0400

    done

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -17,6 +17,13 @@ namespace HelloASPDotNET.Controllers
         {
             string html = "<form method='post' action='/helloworld'>" +
                 "<input type='text' name='name' />" +
+                "<select name='lang'> <option value = 'Welcome to my app, ' > Select a language </option>" +
+                "<option value = 'Hello, ' > English </option>"+
+                "<option value = 'Bonjour, ' > French </option>" +
+                "<option value = 'Hola, ' > Spanish </option>" +
+                "<option value = 'Hallo, ' > German </option>" +
+                "<option value = 'Salve, ' > Latin </option>" +
+                "</select>" +
                 "<input type='submit' value='Greet Me!' />" +
                 "</form>";
 
@@ -25,11 +32,19 @@ namespace HelloASPDotNET.Controllers
 
         // GET: /<controller>/welcome?name=value or GET: /<controller>/welcome/name
         // POST: /<controller>/welcome
-        [HttpGet("welcome/{name?}")]
+        [HttpGet("welcome/{lang}/{name?}")]
         [HttpPost]
-        public IActionResult Welcome(string name = "World")
+        public IActionResult Welcome(string lang, string name = "World")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            return Content("<h1>"+ lang + name + "!</h1>", "text/html");
         }
+
+        //old code
+        //[HttpGet("welcome/{name?}")]
+        //[HttpPost]
+        //public IActionResult Welcome(string name = "World")
+        //{
+        //    return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        //}
     }
 }
